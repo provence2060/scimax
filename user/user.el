bad08c07250b6673e16500c331d1a5ef8ba6c63a
provence2060 <provence2060@163.com>
2020-03-28 13:55:25 +0800

    xelatex  compile

---
+++ user/user.el
@@ -0,0 +1,9 @@
+;; Put your customizations here.
+
+;;; * LaTeX
+(add-hook 'LaTeX-mode-hook
+          (lambda ()
+            (add-to-list 'TeX-command-list '("XeLaTeX" "%`xelatex%(mode)%' %t" TeX-run-TeX nil t))
+            (setq TeX-command-default "XeLaTeX")
+            (setq TeX-save-query  nil )
+            (setq TeX-show-compilation t)))
